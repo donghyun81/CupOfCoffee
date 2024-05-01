420ddb2d8d851babf279663bf512a98bc4df79f5
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-05-01 17:24:03 +0900

    feat: 모임 생성 프래그먼트 및 이동 구현

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("androidx.navigation.safeargs")
 }
 
 android {
@@ -44,6 +45,8 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    implementation("androidx.fragment:fragment-ktx:1.6.2")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
     testImplementation("junit:junit:4.13.2")
--- app/src/main/java/com/cupofcoffee/HomeFragment.kt
+++ app/src/main/java/com/cupofcoffee/HomeFragment.kt
@@ -5,10 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.cupofcoffee.databinding.FragmentHomeBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.CameraUpdate
+import com.naver.maps.map.MapFragment
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
 
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(), OnMapReadyCallback {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
@@ -27,11 +34,51 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        loadMap()
     }
 
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
+    }
+
+    private fun loadMap() {
+        val mapFragment = binding.mapFragment.getFragment<MapFragment>()
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(p0: NaverMap) {
+        setSymbolClick(p0)
+        setCameraZoom(p0)
+    }
+
+    private fun setCameraZoom(naverMap: NaverMap) {
+        naverMap.moveCamera(CameraUpdate.zoomTo(20.0))
+    }
+
+    private fun setSymbolClick(naverMap: NaverMap) {
+        naverMap.setOnSymbolClickListener { symbol ->
+            symbol.caption
+            setDialog(symbol.caption, symbol.position)
+            true
+        }
+    }
+
+    private fun setDialog(placeName: String, position: LatLng) {
+        MaterialAlertDialogBuilder(requireContext())
+            .setTitle(getString(R.string.save_title))
+            .setMessage(placeName)
+            .setNegativeButton(getString(R.string.save_cancle)) { dialog, which -> dialog.cancel() }
+            .setPositiveButton(getString(R.string.save_create)) { dialog, which ->
+                moveToSaveMeeting(placeName,position)
+            }
+            .show()
+    }
+
+    private fun moveToSaveMeeting(placeName: String, position: LatLng) {
+        val action =
+            HomeFragmentDirections.actionHomeFragmentToSaveMeetingFragment(placeName, position)
+        findNavController().navigate(action)
     }
 
 
