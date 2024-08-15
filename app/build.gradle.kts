a85cba7d257a5f9f553fd73b59758e7bea9826db
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-08-16 00:49:08 +0900

    feat: 홈 지도 화면 compose로 이전 및 라이브러리 버전 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -91,7 +91,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
-    implementation("com.naver.maps:map-sdk:3.18.0")
+    implementation("com.naver.maps:map-sdk:3.19.1")
     implementation("com.google.android.gms:play-services-location:21.3.0")
 
     implementation(platform("com.google.firebase:firebase-bom:33.1.2"))
@@ -149,6 +149,10 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.9.1")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.4")
     implementation("androidx.compose.runtime:runtime-livedata")
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
+    implementation ("com.google.accompanist:accompanist-permissions:0.35.1-alpha")
+    implementation ("com.android.volley:volley:1.2.1")
 }
 
 kapt {
