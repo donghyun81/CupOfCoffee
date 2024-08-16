a05393c38c9e09b2f26df559ea211c2a131f65af
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-08-16 17:33:48 +0900

    feat: splash 화면 compose로 변경 및 Color 파일 정의

---
+++ app/src/main/java/com/cupofcoffee0801/ui/graphics/Color.kt
@@ -0,0 +1,9 @@
+package com.cupofcoffee0801.ui.graphics
+
+import androidx.compose.ui.graphics.Color
+
+val Black = Color(0xFF000000)
+val Transparent = Color(0x00000000)
+val White = Color(0xFFFFFFFF)
+val Green = Color(0xFF03C75A)
+val Brown = Color(0xFFddb892)
